98bacd403d5da400a54ab5f586b1b74d4ea4ad9f
Jeremy Herve <jeremy@tagada.hu>
2012-12-20 18:59:05 +0100

    Code Cleanup
    Props @kovshenin

--- jp-subs-shortcode.php
+++ jp-subs-shortcode.php
@@ -10,27 +10,28 @@
  * Text Domain: jetpack
  */
 
-// Check if Jetpack and the subscriptions modules are active
-function tweakjp_sub_shortcode_enable() {
+function tweakjp_sub_shortcode() {
+
+	$args = array(
+		'before_widget' => '<div class="widget jetpack_subscription_widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<div class="widget-title">',
+		'after_title'   => '</div>'
+	);
+	
+	$instance = array(
+		'option' => 'value'
+	);
+
+	// Check if Jetpack and the subscriptions modules are active
 	if (
 		class_exists( 'Jetpack' ) && method_exists( 'Jetpack', 'get_active_modules' ) && in_array( 'subscriptions', Jetpack::get_active_modules() )
 		) {
-		add_shortcode( 'jpsub', 'tweakjp_sub_shortcode' );
+		ob_start();
+		the_widget( 'Jetpack_Subscriptions_Widget', $instance, $args );
+		return ob_get_clean();
 	} else {
-		add_filter( 'the_content', 'tweakjp_sub_rm_shortcode' );
+		return;
 	}
 }
-add_action( 'plugins_loaded', 'tweakjp_sub_shortcode_enable' );
-
-
-// Shortcode
-function tweakjp_sub_shortcode() {
-	ob_start();
-	the_widget( 'Jetpack_Subscriptions_Widget', $instance, $args );
-	return ob_get_clean();
-}
-
-// Remove shortcode from content if any of the modules are not active
-function tweakjp_sub_rm_shortcode( $content ) {
-	return str_replace( '[jpsub]', '', $content );
-}
+add_shortcode( 'jpsub', 'tweakjp_sub_shortcode' );
